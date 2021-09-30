c5432c153904efd3cc5274bf04ce9d3e8a0c0e73
Christopher Wu <cwu267@gmail.com>
2021-09-30 19:36:05 -0400

    add starter code to respository

---
+++ script.js
@@ -0,0 +1,46 @@
+let numRows = 0;
+let numCols = 0;
+let colorSelected; 
+
+//Add a row
+function addR() {
+    alert("Clicked Add Row")
+}
+//Add a column
+function addC() {
+    alert("Clicked Add Col")
+}
+
+//Remove a row
+function removeR() {
+    alert("Clicked Remove Row")
+    if(numRows !== 0)
+    {
+
+    }
+}
+//Remove a column
+function removeC() {
+    alert("Clicked Remove Col")
+    if(numCols !== 0)
+    {
+
+    }
+}
+//sets global var for selected color
+function selected(){
+    colorSelected = document.getElementById("selectedID").value;
+    console.log(colorSelected);
+}
+
+function fill(){
+    alert("Clicked Fill All")
+}
+
+function clearAll(){
+    alert("Clicked Clear All")
+}
+
+function fillU(){
+    alert("Clicked Fill All Uncolored")
+}
